e92b6438e86a444027fcbcdb73a5a58e6608d005
raifernandesz <fernandesdeoliveiradacosta@gmail.com>
2025-05-30 14:54:09 -0300

    Update calculo_de_imc.py

--- calculo_de_imc.py
+++ calculo_de_imc.py
@@ -8,13 +8,13 @@ imc = peso / (altura*altura)
 
 if imc < 18.5:
     print("abaixo do peso")
-elif imc >= 18.5 and peso <= 24.9:
+elif imc >= 18.5 and imc <= 24.9:
     print("peso normal")
-elif imc >=25.0 and peso <= 29.9:
+elif imc >=25.0 and imc <= 29.9:
      print("sobrepeso")
-elif imc >= 30.0 and peso <=34.9:
+elif imc >= 30.0 and imc <=34.9:
     print("Obesidade Grau I")
-elif imc >= 35.0 and peso <= 39.9:
+elif imc >= 35.0 and imc <= 39.9:
     print("Obesidade Grau II")
 else: 
      print("Obesidade Grau III mórbida")
